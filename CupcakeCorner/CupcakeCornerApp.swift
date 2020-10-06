ebcabde3bb0c711c52f279272432a18b58154685
Paul Houghton <paul.houghton@gmail.com>
2020-10-06 08:25:34 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Paul Houghton on 06/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
