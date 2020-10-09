eb43e95d6231ef64cfa258f803892dab44ce7da2
Paul Houghton <paul.houghton@gmail.com>
2020-10-09 07:55:28 +0100

    Initial project.

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,10 +7,137 @@
 
 import SwiftUI
 
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Paul Hudson"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @ObservedObject var order = Order()
+    
+    
+//    @State private var results = [Result]()
+    
+//    @State var username = ""
+//    @State var email = ""
+//
+//    var disableForm: Bool {
+//        username.count < 5 || email.count < 5
+//    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+// Fix for foreach error if it shows...
+//                        ForEach(0..<Order.types.count, id: \.self) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                    
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+                    
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting ) {
+                            Text("Add extra frosting")
+                        }
+                        
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery Details")
+                    }
+                }
+            
+            }
+            .navigationBarTitle("Cupcake Corner")
+        }
+        
+        
+//        List(results, id:\.trackId) { item in
+//            VStack(alignment: .leading) {
+//                Text(item.trackName)
+//                    .font(.headline)
+//
+//                Text(item.collectionName)
+//            }
+//        }
+//        .onAppear(perform: loadData)
+    
+//    func loadData() {
+//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+//            print("Invalid URL")
+//            return
+//        }
+//
+//        let request = URLRequest(url: url)
+//
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+//                    DispatchQueue.main.async {
+//                        self.results = decodedResponse.results
+//                    }
+//
+//                    return
+//                }
+//            }
+//            print("Fetch failed: \(error?.localizedDescription ?? "Unknown Error")")
+//
+//        }.resume()
+    
+//        Form {
+//            Section {
+//                TextField("Username", text: $username)
+//                TextField("Email", text: $email)
+//            }
+//
+//            Section {
+//                Button("Create account") {
+//                    print("Creating account...")
+//                }
+//            }
+//            .disabled(disableForm)
+//        }
     }
 }
 
